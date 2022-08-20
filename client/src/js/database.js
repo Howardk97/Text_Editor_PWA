f7ee222f52de51425cf7d60bbfb2e9afc3985f74
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-19 21:54:36 -0400

    Added logic to event listeners

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,40 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // create database
+  openDB('editor', 1, {
+    upgrade(db) {
+      if(db.objectStoreNames.contains('editor')) {
+        console.log('editor database already exists')
+        return;
+      }
+
+      // Object store for data
+      db.createObjectStore('editor', { keyPath: 'id', autoIncrement: true });
+      console.log('editor database created')
+    }
+  })
+  console.error('putDb not implemented')
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // connect to database
+  const editorDb = await openDB('editor', 1);
 
+  // Specify database and privleges
+  const tx = contactDb.trahnsaction('contact', 'readonly');
+
+  // Get data from database
+  const request = store.getAll();
+
+  // Get confirmation of request
+  const result = await request;
+  console.log(result);
+
+  console.error('getDb not implemented')
+};
+
+// Starts the database
 initdb();
